8a593c3d1a7616b86c375acd43741e2a0875d1ea
Alex Gura <guraaleksandr18@gmail.com>
2023-10-25 18:53:48 +0700

    2.3. Added contact status.

--- Module2/2/1/contacts.c
+++ Module2/2/1/contacts.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+
 #define LEN 30
 #define PHONE_NUMBER 5
 #define EMAIL_NUMBER 3
 #define CONTACTS_NUMBER 5
 
-struct Contact
+typedef enum Status {Active = 1, Inactive = -1} Status;
+
+typedef struct Contact
 {
+	Status stat;
 	char full_name[LEN];
 	char work_place[LEN];
 	char work_post[LEN];
-	char phone_book[PHONE_NUMBER][LEN];
-	char email[EMAIL_NUMBER][LEN];
+	char phone_book[LEN];
+	char email[LEN];
 	char social_net[LEN];
-};
+} Contact;
 
-int menu(struct Contact* conts);
-void print_contacts(struct Contact* conts, int count);
-void add_contact(struct Contact* conts, int n);
+int menu(Contact* conts);
+void print_contacts(Contact* conts);
+void add_contact(Contact* conts);
 void remove_contact();
 void change_contact();
 
 int main()
 {
-	struct Contact contacts[CONTACTS_NUMBER];
+	Contact contacts[CONTACTS_NUMBER];
+	contacts[0].stat = Active;
+	strncpy(contacts[0].full_name, "Alex Gura", LEN);
+
+	for (int i = 0; i < CONTACTS_NUMBER; i++)
+		contacts[i].stat = Inactive;
 
 	int active = menu(contacts);
 	while (active != 0)
@@ -32,7 +42,7 @@ int main()
 	return 0;
 }
 
-int menu(struct Contact* conts)
+int menu(Contact* conts)
 {
 	printf("Enter your choice (q - to quit):\n");
 	printf("0. Print all contacts;\n");
@@ -44,10 +54,10 @@ int menu(struct Contact* conts)
 	switch(choice)
 	{
 		case '0':
-			print_contacts(conts, 2);
+			print_contacts(conts);
 			break;
 		case '1':
-			add_contact(conts, 1);
+			add_contact(conts);
 			break;
 		case '2':
 			remove_contact();
@@ -62,18 +72,32 @@ int menu(struct Contact* conts)
 	return 1;
 }
 
-void print_contacts(struct Contact* conts, int count)
+void print_contacts(Contact* conts)
 {
-	for (int i = 0; i < count; i++)
+	for (int i = 0; i < CONTACTS_NUMBER; i++)
 	{
-		printf("Full name: %s", (*(conts + i)).full_name);
+		if (conts[i].stat == Active)
+		{
+			printf("Full name - %s", conts[i].full_name);
+			printf("Work place - %s", conts[i].work_place);
+			printf("Work post - %s", conts[i].work_post);
+			printf("Phone number - %s", conts[i].phone_book);
+			printf("Email - %s", conts[i].email);
+			printf("Social network - %s", conts[i].social_net);
+		}
 	}
 }
 
-void add_contact(struct Contact* conts, int n)
+void add_contact(Contact* conts)
 {
+	int index = 0;
+	for (int i = 0; i < CONTACTS_NUMBER; i++)
+		if (conts[i].stat != Active)
+			index++;
+
+	conts[index].stat = Active;
 	printf("Enter your full name: ");
-	fgets((*(conts + n)).full_name, LEN, stdin);
+	fgets(conts[index].full_name, LEN, stdin);
 	printf("Enter your work place: ");
 	printf("Enter your work post: ");
 	printf("Enter your %d numbers: ", PHONE_NUMBER);
