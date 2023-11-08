db77dc821ed0098d296340a888205e6a66bcf2e9
Alex Gura <guraaleksandr18@gmail.com>
2023-11-08 15:34:32 +0700

    2.2. Refactoring.

--- Module2/2/2/operations.c
+++ Module2/2/2/operations.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "operations.h"
 
+static void clear_input()
+{
+	for (char c = getchar(); c != '\n' && c != EOF; c = getchar());
+}
+
 void add(int x, int y)
 {
 	printf("%d + %d = %d\n\n", x, y, x + y);
@@ -24,6 +29,37 @@ void div(int x, int y)
 		printf("%d / %d = %d\n\n", x, y, x / y);
 }
 
+int select(char choice, int *f, int *s)
+{
+	switch(choice)
+	{
+		case '+':
+			add(*f, *s);
+			return 1;
+
+		case '-':
+			sub(*f, *s);
+			return 1;
+
+		case '*':
+			mult(*f, *s);
+			return 1;
+
+		case '/':
+			div(*f, *s);
+			return 1;
+
+		case 'Q':
+		case 'q':
+			printf("Good bye, have a good day!\n");
+			return 0;
+
+		default:
+			printf("\nInvalid choice!\nEnter again: ");
+			return -1;
+	}
+}
+
 int menu()
 {
 	int first, second;
@@ -31,27 +67,19 @@ int menu()
 	printf("Enter 'q' to end calc.\n");
 	printf("Enter the firts number: ");
 	scanf("%d", &first);
+	clear_input();
 
 	printf("Enter the second number: ");
 	scanf("%d", &second);
+	clear_input();
 
 	printf("Enter the operation(+ - * /): ");
-	getchar();
-	char choice = getchar();
-	getchar();
-
-	switch(choice)
+	char choice, state;
+	do
 	{
-		case '+':
-			add(first, second); return 1;
-		case '-':
-			sub(first, second); return 1;
-		case '*':
-			mult(first, second); return 1;
-		case '/':
-			div(first, second); return 1;
-		default:
-			return 0;
-	}
+		choice = getchar();
+		clear_input();
+		state = select(choice, &first, &second);
+	} while (state == -1);
 }
 
--- Module2/2/2/operations.h
+++ Module2/2/2/operations.h
@@ -1,10 +1,13 @@
 #ifndef OPERATIONS_H_
 #define OPERATIONS_H_
 
+static void clear_input();
+
 void add(int, int);
 void sub(int, int);
 void mult(int, int);
 void div(int, int);
+int select(char, int*, int*);
 int menu();
 
 #endif
