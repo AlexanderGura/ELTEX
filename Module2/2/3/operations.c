be424631e687beedcabf503f44f06fb1ed6fa604
Alex Gura <guraaleksandr18@gmail.com>
2023-11-09 13:53:43 +0700

    2.3. Refactoring.

--- Module2/2/3/operations.c
+++ Module2/2/3/operations.c
@@ -2,21 +2,64 @@
 #include <math.h>
 #include "operations.h"
 
+static void clear_input(void)
+{
+	for (char c = getchar(); c != '\n' && c != EOF; c = getchar());
+}
+
+static void (*select(char choice))()
+{
+	switch(choice)
+	{
+		case '1':
+		case '+':
+			return add;
+		case '2':
+		case '-':
+			return sub;
+		case '3':
+		case '*':
+			return mult;
+		case '4':
+		case '/':
+			return div;
+		case '5':
+		case '^':
+			return power;
+		case '6':
+		case 'r':
+			return sqroot;
+		case '7':
+		case 'l':
+			return natlog;
+		case '0':
+		case 'q':
+		case 'Q':
+			printf("Good bye, have a good day!\n");
+			return NULL;
+		default:
+			printf("Invalid choice!\n");
+			return NULL;
+		}
+}
+
 int menu()
 {
-	printf("\nEnter the number of operation:\n");
-	printf("Enter '0' to end the calc!\n");
-	printf("1. +\n2. -\n3. *\n4. /\n5. power\n6. sqrt\n7. ln\n");
+	printf("\nEnter '0' or 'q' to end the calc!\n");
+	printf("Enter the number of operation:\n");
+	printf("1(+). +\n2(-). -\n3(*). *\n4(/). /\n");
+	printf("5(^). power\n6(r). sqrt\n7(l). ln\n");
+	printf("Your choice: ");
 
-	int choice;
-	scanf("%d", &choice);
-	getchar();
-	if (choice == 0)
+	char choice;
+	choice = getchar();
+	clear_input();
+
+	void (*operation)(void) = select(choice);
+	if (operation == NULL)
 		return 0;
 
-	void (*operations[OPERATIONS_NUM]) (void) = {
-		add, sub, mult, div, power, sqroot, natlog};
-	operations[choice - 1]();
+	operation();
 	return 1;
 }
 
@@ -24,38 +67,38 @@ void get_one_arg(float* num)
 {
 	printf("Enter the number: ");
 	scanf("%f", num);
-	getchar();
+	clear_input();
 }
 
 void get_two_arg(float* first, float* second)
 {
 	printf("Enter the first number: ");
 	scanf("%f", first);
-	getchar();
+	clear_input();
 
 	printf("Enter the second number: ");
 	scanf("%f", second);
-	getchar();
+	clear_input();
 }
 
 void add(void)
 {
 	float x, y;
 	get_two_arg(&x, &y);
-	printf("%.2f + %.2f = %.2f\n\n", x, y, x + y);
+	printf("\t%.2f + %.2f = %.2f\n\n", x, y, x + y);
 }
 void sub(void)
 {
 	float x, y;
 	get_two_arg(&x, &y);
-	printf("%.2f - %.2f = %.2f\n\n", x, y, x - y);
+	printf("\t%.2f - %.2f = %.2f\n\n", x, y, x - y);
 }
 
 void mult(void)
 {
 	float x, y;
 	get_two_arg(&x, &y);
-	printf("%.2f * %.2f = %.2f\n\n", x, y, x * y);
+	printf("\t%.2f * %.2f = %.2f\n\n", x, y, x * y);
 }
 
 void div(void)
@@ -65,14 +108,14 @@ void div(void)
 	if (y == 0)
 		printf("ERROR: division by zero!\n\n");
 	else
-		printf("%.2f / %.2f = %.2f\n\n", x, y, x / y);
+		printf("\t%.2f / %.2f = %.2f\n\n", x, y, x / y);
 }
 
 void power(void)
 {
 	float x, y;
 	get_two_arg(&x, &y);
-	printf("%.2f^%.2f = %.2f\n\n", x, y, pow(x, y));
+	printf("\t%.2f^%.2f = %.2f\n\n", x, y, pow(x, y));
 }
 
 void sqroot(void)
@@ -82,7 +125,7 @@ void sqroot(void)
 	if (x < 0)
 		printf("ERROR, sqrt's argument couldn't be negative!\n");
 	else
-		printf("sqrt(%.2f) = %.2f\n", x, sqrt(x));
+		printf("\tsqrt(%.2f) = %.2f\n", x, sqrt(x));
 }
 
 
@@ -93,5 +136,5 @@ void natlog(void)
 	if (x < 0)
 		printf("ERROR, ln's argument couldn't be negative!\n\n");
 	else
-		printf("ln(%.2f) = %.2f\n\n", x, log(x));
+		printf("\tln(%.2f) = %.2f\n\n", x, log(x));
 }
--- Module2/2/3/operations.h
+++ Module2/2/3/operations.h
@@ -3,7 +3,10 @@
 
 #define OPERATIONS_NUM 7
 
-int menu();
+static void clear_input(void);
+static void (*select(char choice))(void);
+
+int menu(void);
 void get_one_arg(float *);
 void get_two_arg(float*, float*);
 void add(void);
