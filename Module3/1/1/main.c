cb02baeb312d8e019b24ec858ddb0068aad54de3
Alex Gura <guraaleksandr18@gmail.com>
2023-11-15 16:37:21 +0700

    1.1. Finall version!

---
+++ Module3/1/1/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void goodbye(void)
+{
+	printf("See you later! Bye...\n");
+}
+
+void hello(int num, void *args)
+{
+	printf("%d\n", num);
+	printf("%p\n", args);
+}
+
+int main(int argc, char *argv[])
+{
+	pid_t pid = fork();
+
+	int state = atexit(goodbye);
+	if (state != 0)
+	{
+		fprintf(stderr, "cannot set exit function!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	switch(pid)
+	{
+		case -1:
+			printf("Error\n");
+			exit(EXIT_FAILURE);
+		case 0:
+			printf("\nI'm a child(%d)!\n", getpid());
+			for (int i = 1; i < argc; i++)
+				printf("%d: %s\n", i, argv[i]);
+			exit(EXIT_SUCCESS);
+		default:
+			printf("\nI'm a parent(%d)!\n", getpid());
+			for (int i = 1; i < argc; i++)
+				printf("%d: %s\n", i, argv[i]);
+			exit(EXIT_SUCCESS);
+	}
+}
