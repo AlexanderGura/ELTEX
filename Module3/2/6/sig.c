d35dcd4ebe79dc0657295d9abf951287a975ffdb
Alex Gura <guraaleksandr18@gmail.com>
2023-11-21 17:19:08 +0700

    2.6. Final version.

---
+++ Module3/2/6/sig.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+
+int count = 0;
+
+void handler(int sig)
+{
+	count++;
+	if (count == 3)
+		exit(EXIT_SUCCESS);
+}
+
+int main()
+{
+	int fd = open("signal.txt", O_RDWR | O_CREAT, 0664);
+	if (fd == -1)
+	{
+		fprintf(stderr, "Couln't open file!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (signal(SIGINT, handler) == SIG_ERR)
+		exit(EXIT_FAILURE);
+
+	int count = 1;
+	while (1)
+	{
+		if (write(fd, &count, sizeof(count)) == -1)
+			exit(EXIT_FAILURE);
+		count++;
+		sleep(1);
+	}
+
+	if (close(fd) == -1)
+	{
+		fprintf(stderr, "Couln't close file!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	exit(EXIT_SUCCESS);
+}
